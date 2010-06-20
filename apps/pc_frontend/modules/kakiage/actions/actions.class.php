ee0da3e983ac95420faeca9d6f36f8525fbdae7a
Rimpei Ogawa <ogawa@tejimaya.com>
2010-06-21 01:43:29 +0900

    generate plugin files

---
+++ apps/pc_frontend/modules/kakiage/actions/actions.class.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * kakiage actions.
+ *
+ * @package    OpenPNE
+ * @subpackage kakiage
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
+ */
+class kakiageActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfWebRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
