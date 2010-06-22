4f6b2eeed38d7ace41e133b68e1edcf1ee2230d9
mtakai <m.takai@tejimaya.com>
2010-06-22 19:39:49 +0800

    add CSS and modify template

--- apps/pc_frontend/modules/kakiage/templates/indexSuccess.php
+++ apps/pc_frontend/modules/kakiage/templates/indexSuccess.php
@@ -6,15 +6,19 @@
 <p class="next"><?php echo link_to(__('Next Day'), '@kakiage_date?'.strftime('year=%Y&month=%m&day=%d', strtotime($date.' +1 day'))) ?></p>
 </div>
 
+<div id="topEditLink" class="editLink"><?php echo link_to(__('Edit'), 'kakiage/edit') ?></div>
+
 <?php foreach ($list as $item): ?>
-<div class="kakiage">
+<div id="id_<?php echo $item['Member']->getId() ?>" class="kakiage">
+<?php if ($sf_user->getMemberId() === $item['Member']->getId()): ?>
+<div class="editLink"><?php echo link_to(__('Edit'), 'kakiage/edit') ?></div>
+<?php endif; ?>
 <div class="kakiage_member"><?php echo op_link_to_member($item['Member']) ?></div>
-<div class="kakiage_body"><?php echo op_url_cmd(op_decoration(nl2br($item['body']))) ?></div>
 <div class="kakiage_updated_at">(<?php echo __('Updated at') ?>: <?php echo op_format_date($item['updated_at'], 'XDateTimeJa') ?>)</div>
+<div class="kakiage_body"><?php echo op_url_cmd(op_decoration(nl2br($item['body']))) ?></div>
 </div>
 <?php endforeach ?>
-</div>
 
-<div class="parts">
-<?php echo link_to(__('Edit'), 'kakiage/edit') ?>
+<div id="bottomEditLink" class="editLink"><?php echo link_to(__('Edit'), 'kakiage/edit') ?></div>
+
 </div>
