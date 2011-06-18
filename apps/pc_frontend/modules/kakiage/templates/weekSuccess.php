5e32c4fa5ed1448a9b7aeb3683d086b5489caa31
Rimpei Ogawa <ogawa@tejimaya.com>
2011-06-19 08:23:45 +0900

    add edit link to the recent and week page

--- apps/pc_frontend/modules/kakiage/templates/weekSuccess.php
+++ apps/pc_frontend/modules/kakiage/templates/weekSuccess.php
@@ -5,6 +5,7 @@
 <div id="id_<?php echo $item['target_date'] ?>" class="kakiage">
 <div class="kakiage_member"><?php echo date('Y-m-d (D)', strtotime($item['target_date'])) ?></div>
 <div class="kakiage_updated_at">(<?php echo __('Updated at') ?>: <?php echo op_format_date($item['updated_at'], 'XDateTimeJa') ?>)</div>
+<div class="editLink"><?php echo link_to(__('Edit'), '@kakiage_edit_date?'.strftime('year=%Y&month=%m&day=%d', strtotime($item['target_date']))) ?></div>
 <div class="kakiage_body"><?php echo op_url_cmd(op_decoration(nl2br($item['body']))) ?></div>
 </div>
 <?php endforeach ?>
