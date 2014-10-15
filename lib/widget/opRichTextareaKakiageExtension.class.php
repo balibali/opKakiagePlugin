81b1079fa047424cdae2f37ad7b3d370039b38b6
isaosano <sano@tejimaya.com>
2014-10-15 13:47:12 +0900

    correspond to version 3.8

--- lib/widget/opRichTextareaKakiageExtension.class.php
+++ lib/widget/opRichTextareaKakiageExtension.class.php
@@ -19,8 +19,8 @@ class opRichTextareaKakiageExtension extends opWidgetFormRichTextareaOpenPNEExte
   static public function getButtonOnClickActions()
   {
     return array(
-      'op_kakiage_copy_from_previous_day' => 'var a=$("kakiage_body"),b=$$("textarea.kakiage_body")[0];a.setValue($F(a)?$F(a)+"\n"+$F(b):$F(b));',
-      'op_kakiage_nocall' => 'var a=$("kakiage_body");/^%%nocall/.test($F(a))||a.setValue("%%nocall\n"+$F(a));',
+      'op_kakiage_copy_from_previous_day' => 'var a=$("#kakiage_body"),b=$("textarea.kakiage_body").val();a.val(a.val()+"\n"+b);',
+      'op_kakiage_nocall' => 'var a=$("#kakiage_body");a.val("%%nocall"+"\n"+a.val());',
     );
   }
 }
