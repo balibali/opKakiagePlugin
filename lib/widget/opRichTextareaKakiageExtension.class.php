6381e91b27d1c4c09d78915f05dafba63004c908
isaosano <sano@tejimaya.com>
2014-10-15 14:00:03 +0900

    Fixed line breaks the copy button

--- lib/widget/opRichTextareaKakiageExtension.class.php
+++ lib/widget/opRichTextareaKakiageExtension.class.php
@@ -19,7 +19,7 @@ class opRichTextareaKakiageExtension extends opWidgetFormRichTextareaOpenPNEExte
   static public function getButtonOnClickActions()
   {
     return array(
-      'op_kakiage_copy_from_previous_day' => 'var a=$("#kakiage_body"),b=$("textarea.kakiage_body").val();a.val(a.val()+"\n"+b);',
+      'op_kakiage_copy_from_previous_day' => 'var a=$("#kakiage_body"),b=$("textarea.kakiage_body").val();a.val(a.val()+b+"\n");',
       'op_kakiage_nocall' => 'var a=$("#kakiage_body");a.val("%%nocall"+"\n"+a.val());',
     );
   }
